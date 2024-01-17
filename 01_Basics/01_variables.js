f0b5df2a11bd4549d624956b10ffeca3ef99487f
NITESH KUMAR <126846212+henrynitesh789@users.noreply.github.com>
2024-01-17 19:46:01 +0000

    variables in js

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const customerId = 12353
+let email = "niteshkumr78768@gmail.com"
+var password = "23343"
+city = "delhi" // this is a possibility in js but not a good practice
+let state;
+console.log(customerId);
+/* prefer not to use var because of issue in block scope and functional scope */
+
+email = "bhgh@bvgv.com"
+password = "2765"
+city = "noida"
+
+console.table([email,password,city,state])
