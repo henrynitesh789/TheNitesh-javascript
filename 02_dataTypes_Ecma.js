1bb0ab2d84cc0e6e9956a7648a69a6398b1d179f
NITESH KUMAR <126846212+henrynitesh789@users.noreply.github.com>
2024-01-18 04:56:39 +0000

    data types in js

---
+++ 02_dataTypes_Ecma.js
@@ -0,0 +1,25 @@
+"use strict";  // treat all JS code as newer version
+//alert("welcom")  // we are using nodejs, not browser
+
+let name = "nitesh"
+let age = 18
+let mature = false
+let state = null
+
+/* PRIMITIVE DATA TYPES
+
+    number  -> 2 to power 53
+    biginit -> bigger integer
+    string  -> use in double quotes (prefered)
+    boolean -> true/false
+    null    -> standalone value
+    undefined ->
+    symbol -> unique */
+
+/* NON-PRIMITIVE DATA TYPES
+    object     */
+
+console.log(typeof "nitesh");
+console.log("age");
+console.log(typeof null);
+console.log(typeof undefined);
