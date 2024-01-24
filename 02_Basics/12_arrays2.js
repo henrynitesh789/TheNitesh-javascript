9efb2c86fd105b879021ccdabbec38847934a76c
NITESH KUMAR <126846212+henrynitesh789@users.noreply.github.com>
2024-01-24 07:52:26 +0000

    arrays method

---
+++ 02_Basics/12_arrays2.js
@@ -0,0 +1,10 @@
+// +++++++++++ Array2 +++++++++
+const founder = ["ratan tata", "bill gates", "setve jobs"];
+const ceo = ["sundar pichai", "stya nadela", "parag agarwal"]
+
+founder.push(ceo) // ceo array inside founder array as an element
+// console.log(founder);
+// console.log(founder[3][0]);
+const All = founder.concat(ceo)  // this function combine array and return a new arrya
+console.log(All); // same as push function
+
