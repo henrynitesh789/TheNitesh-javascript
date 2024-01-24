99eb6bf74ce1daa504d60c9b6bd913a3ab5eba29
NITESH KUMAR <126846212+henrynitesh789@users.noreply.github.com>
2024-01-24 23:52:44 +0530

    arrays modified

--- 02_Basics/12_arrays2.js
+++ 02_Basics/12_arrays2.js
@@ -2,9 +2,10 @@
 const founder = ["ratan tata", "bill gates", "setve jobs"];
 const ceo = ["sundar pichai", "stya nadela", "parag agarwal"]
 
-founder.push(ceo) // ceo array inside founder array as an element
-// console.log(founder);
-// console.log(founder[3][0]);
-const All = founder.concat(ceo)  // this function combine array and return a new arrya
-console.log(All); // same as push function
+// founder.push(ceo) // ceo array inside founder array as an element
+// // console.log(founder);
+// // console.log(founder[3][0]);
+// const All = founder.concat(ceo)  // this function combine array and return a new array
+// console.log(All); // same as push function
 
+const allNew = [...founder, ...ceo]
