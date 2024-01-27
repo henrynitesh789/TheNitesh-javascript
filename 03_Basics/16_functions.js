6d02a195c7c100bf643d70c92b4e11a33741eeed
NITESH KUMAR <126846212+henrynitesh789@users.noreply.github.com>
2024-01-27 10:44:09 +0000

    function in js

---
+++ 03_Basics/16_functions.js
@@ -0,0 +1,28 @@
+// // Functions and parameter
+
+// function addNumber(num1, num2){
+//     console.log(num1 + num2);
+// }
+// // calling function
+// addNumber() // output: NaN
+// addNumber(3,4) // with argument  output: 7
+// addNumber(3, "4")  // output: 34
+// addNumber(3, "a")  // output: 3a
+// addNumber(3, null)  // output: 3
+
+// const result = addNumber(3,5)
+// console.log("result: ", result);  // output: undefined
+
+// function add(num1, num2){
+//     return num1 + num2
+// }
+// console.log("sum is: ", add(3,9));  // output: undefined
+
+function loginUsermesssage(username, ){
+    return `${username} just logged in`
+}
+// calling 
+console.log(loginUsermesssage("hjjsahjj@gmail.com"));
+console.log(loginUsermesssage(""));  // output: just logged in
+console.log(loginUsermesssage());  // output: undefined just logged in
+
