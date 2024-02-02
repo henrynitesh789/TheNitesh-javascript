1380d10f94134ec4e31739f8111a86121aa9dd9e
NITESH KUMAR <126846212+henrynitesh789@users.noreply.github.com>
2024-02-02 03:55:07 +0000

    while and do-while loop in js

---
+++ 05_Iterations/26_while_and_do_While.js
@@ -0,0 +1,24 @@
+
+/* Syntax: while loop
+
+    while (condition) {
+        ----------
+    }
+*/
+let i = 0
+while (i <= 2){
+    console.log(i);
+    i++;
+}
+
+/* Syntax: do-while loop
+
+    do {
+        -----------
+    } while (condition);
+*/
+let score = 11
+do {
+    console.log("score is", score);
+    score++
+} while (score < 4);
